ab80d6485e93eb3293c9a83b69c03cd440da8ce7
Evstafiy <lefteryadi@mail.ru>
2024-07-04 19:21:40 +0300

    server done

--- server/db/models/ask.js
+++ server/db/models/ask.js
@@ -4,13 +4,8 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Ask extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-		this.belongsTo(models.Category, { foreignKey: 'owner_id' }); 
+		this.belongsTo(models.Category, { foreignKey: 'category_id' }); 
     }
   }
   Ask.init({
